a6c10b649fcce9888cf73bd9d2e81c10af321cd2
NovellaRutigliano <novella.rutigliano@gmail.com>
2026-01-15 11:39:21 +0100

    aggiunte le unità di misura dei campi

--- VirtualLab_MatLab/TokaPlot_MATLAB/TokaPlot_tests.m
+++ VirtualLab_MatLab/TokaPlot_MATLAB/TokaPlot_tests.m
@@ -2,61 +2,101 @@ clear
 clc
 close all
 
+clear; clc;
 
-% load equilibrium (calculated from SimPla)
+addpath SimPla_Matlab\functions\
+addpath SimPla_Matlab\tokamaks\geometry\
+addpath SimPla_Matlab\tokamaks\equilibrium\
+addpath SimPla_Matlab\tokamaks\kinetic\
 
-machine = "Tokalab";
-paths = SynDiag_init(machine);
+% initialise the class tokamak
+tok = tokamak;
 
-addpath("equilibrium\")
-load("Tokalab_equi_scenario1.mat")
+% upload the geometry information of your tokamak
+tok = tok.machine_upload();
+tok = tok.scenario_upload(3,1);
+tok = tok.kinetic_upload();
 
-% run your diagnostics
+% initialise the class geometry
+geo = geometry;
+geo = geo.import_geometry(tok);
+geo = geo.build_geometry();
+geo = geo.inside_wall();
 
-PickUp = Diag_PickUpCoils();
-PickUp = PickUp.Upload(1);
-PickUp = PickUp.measure(equi);
+% initialise the class equilibrium
+equi = equilibrium;
+equi = equi.import_configuration(geo,tok.config);
+equi = equi.import_classes();
+equi.separatrix = equi.separatrix.build_separatrix(equi.config.separatrix,equi.geo);
 
-FluxLoops = Diag_FluxLoops();
-FluxLoops = FluxLoops.Upload(1);
-FluxLoops = FluxLoops.measure(equi);
+% solve equilibrium
+equi = equi.solve_equilibrium();
 
-SaddleCoils = Diag_SaddleCoils();
-SaddleCoils = SaddleCoils.Upload(1);
-SaddleCoils = SaddleCoils.measure(equi);
+% post processing (Opoint, Xpoint, LFCS)
+equi = equi.equi_pp();
 
-TS = Diag_ThomsonScattering();
-TS = TS.Upload(1);
-TS = TS.measure(equi);
+% mhd and kinetic profiles
+equi  = equi.compute_profiles();
 
-IntPol = Diag_InterferometerPolarimeter();
-IntPol = IntPol.Upload(1);
-IntPol = IntPol.measure(equi);
 
-Bolo = Diag_Bolo();
-Bolo  = Bolo.Upload(1);
-Bolo = Bolo.measure(equi);
-
+% % load equilibrium (calculated from SimPla)
+% 
+% machine = "Tokalab";
+% paths = SynDiag_init(machine);
+% 
+% addpath("equilibrium\")
+% load("Tokalab_equi_scenario1.mat")
+% 
+% % run your diagnostics
+% 
+% PickUp = Diag_PickUpCoils();
+% PickUp = PickUp.Upload(1);
+% PickUp = PickUp.measure(equi);
+% 
+% FluxLoops = Diag_FluxLoops();
+% FluxLoops = FluxLoops.Upload(1);
+% FluxLoops = FluxLoops.measure(equi);
+% 
+% SaddleCoils = Diag_SaddleCoils();
+% SaddleCoils = SaddleCoils.Upload(1);
+% SaddleCoils = SaddleCoils.measure(equi);
+% 
+% TS = Diag_ThomsonScattering();
+% TS = TS.Upload(1);
+% TS = TS.measure(equi);
+% 
+% IntPol = Diag_InterferometerPolarimeter();
+% IntPol = IntPol.Upload(1);
+% IntPol = IntPol.measure(equi);
+% 
+% Bolo = Diag_Bolo();
+% Bolo  = Bolo.Upload(1);
+% Bolo = Bolo.measure(equi);
+% 
 %%
 clc
 
 TP = TokaPlot;
-
-% fig.config.psi_lines = [0.88 0.9 0.99 1 1.01 1.1];
+close all
+clf
+ 
+figura.config.psi_lines = [0.5 0.66 0.7 0.75 0.9 0.95 1 1.1];
 figura.config.subplot = [1 1 1];
-figura.config.plot_wall = 0;
-figura2 = figure();
-figura.config.hold = "on";
+figura.config.plot_wall = 1;
+% figura.config.hold = "on";
+figura.fig = figure();
 
+figura = TP.PlotField(equi,"ne", figura.fig, figura.config);
 
-% fig2.fig = figure();
+figura2.config.psi_lines = [0.5 0.66 0.7 0.75 0.9 0.95 1 1.1];
+figura2.config.subplot = [1 1 1];
+figura2.config.plot_wall = 1;
+% figura.config.hold = "on";
+figura2.fig = figure();
+
+figura2 = TP.PlotField(equi,"psi", figura2.fig, figura2.config);
+% figura2 = TP.PlotDiagnostics(equi,IntPol, figura2, figure2.config);
 % 
-figure2.config.plot_wall = 1;
-% figure2.config.hold = "on";
-
-figura2 = TP.PlotField(equi,"ne", figura2, figura.config);
-figura2 = TP.PlotDiagnostics(equi,IntPol, figura2, figure2.config);
-
-% clf
-
-figura2 = TP.PlotMeasurements(Bolo,"prj",figura2,figure2.config)
+% % clf
+% 
+% figura2 = TP.PlotMeasurements(Bolo,"prj",figura2,figure2.config)
