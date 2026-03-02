5fd0c38284533f96675c1c9f086e7f1b02eb4dbe
Riccardo Rossi <riccardo.rossi.en@gmail.com>
2026-03-02 15:09:12 +0100

    Rename package to 'VirtualLab' and update dependencies

--- VirtualLab_Python/setup.py
+++ VirtualLab_Python/setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tokalab",
+    name="VirtualLab",            
     version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
+    packages=find_packages(),     
+    install_requires=[            
         "numpy",
-        "matplotlib",
         "scipy",
-        "streamlit",  # se lo usi
-        # altre dipendenze
+        "matplotlib",
+        "streamlit"
     ],
 )
